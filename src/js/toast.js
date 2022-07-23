3c59bfddd3db7ea30d22db3ab9acddddd4589df1
Hed40 <90209761+Hed40@users.noreply.github.com>
2022-07-24 00:59:14 +0200

    Fix: Bug modal + card

---
+++ src/js/toast.js
@@ -0,0 +1,9 @@
+const toastTrigger = document.getElementById('logo_info')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
